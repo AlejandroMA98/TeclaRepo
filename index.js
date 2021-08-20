a503fb94f944531a0e1dffe385a7ab82f6e2fcf3
Alejandro Montañez <alejandroma98123@gmail.com>
2021-08-19 19:52:06 -0500

    se crean funciones asincronas

---
+++ index.js
@@ -0,0 +1,16 @@
+// function llamada(){
+//     console.log("hola we");
+// }
+// function llamadora(call){
+//     console.log("dentro de esta funcion llamdas nuestro callback");
+//     call()
+// }
+// llamadora(llamada);
+
+console.log("primero");
+setTimeout(() =>{
+    console.log("segundo");
+}
+
+)
+console.log("tercero");
